0c4b1f5114b69bb943cce442c7151c03ce971b2b
Joe Sylve <joe@blackbagtech.com>
2015-08-08 13:22:39 -0500

    Added solution for lab 1

--- timestamp/filetime.go
+++ timestamp/filetime.go
@@ -7,14 +7,20 @@ import "time"
 // January 1, 1601 Coordinated Universal Time (UTC).
 type FileTime int64
 
+const (
+	nsPerSec   int64 = 10000000    // Number of 100 Nano Seconds per second
+	secToEpoch int64 = 11644473600 // Seconds between January 1, 1601 and Epoch
+)
+
 // Time returns t as a standard time.Time
 func (t FileTime) Time() time.Time {
-	// TODO: Implement me!
-	return time.Time{}
+	sec := (int64(t) / (nsPerSec)) - secToEpoch
+	return time.Unix(sec, 0)
 }
 
 // ToFileTime returns t as a FileTime
 func ToFileTime(t time.Time) FileTime {
-	// TODO: Implement me!
-	return FileTime(0)
+	sec := (t.Unix() + secToEpoch) * nsPerSec
+
+	return FileTime(sec)
 }
